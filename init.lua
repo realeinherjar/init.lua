e533dd070cfb4a0921d5e7e8a92168959b907583
Einherjar <realeinherjar@proton.me>
2023-09-07 11:28:10 -0400

    fix(flash): add treesitter keybindings

--- init.lua
+++ init.lua
@@ -1008,8 +1008,10 @@ require("lazy").setup({
     event = "VeryLazy",
     -- stylua: ignore
     keys = {
-      { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash", },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash", },
+      { "s", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
+      -- { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" }, -- conflicts with nvim.surround `S`.
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     },
   },
 })
